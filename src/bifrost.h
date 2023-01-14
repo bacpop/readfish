9d3cc8177f8dca89840f1f3306eee6ac833fb2c6
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2023-01-14 17:20:11 +0000

    Adds pybind11 headers.

--- src/bifrost.h
+++ src/bifrost.h
@@ -6,6 +6,10 @@
 #include <sstream>
 #include <bifrost/CompactedDBG.hpp>
 
+// pybind11 headers
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
 class Graph {
     public:
 //    // constructors for debugging
